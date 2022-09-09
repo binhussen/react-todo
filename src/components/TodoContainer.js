e0995f2d2411ed36417f6b620e9bd4b90c883860
binhussen <mhdhsna@gmail.com>
2022-09-09 09:57:13 +0300

    Add Todo Container

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,35 @@
+const { Component } = require('react');
+
+class TodoContainer extends Component {
+	state = {
+	  todos: [
+	    {
+	      id: 1,
+	      title: 'Setup development environment',
+	      completed: true,
+	    },
+	    {
+	      id: 2,
+	      title: 'Develop website and add content',
+	      completed: false,
+	    },
+	    {
+	      id: 3,
+	      title: 'Deploy to live server',
+	      completed: false,
+	    },
+	  ],
+	};
+
+	render() {
+	  return (
+  <ul>
+    {this.state.todos.map((todo) => (
+      <li key={todo.id}>{todo.title}</li>
+    ))}
+  </ul>
+	  );
+	}
+}
+
+export default TodoContainer;
